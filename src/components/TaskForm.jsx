585a10a1289edf00917678cbf6949ad635bc4161
dapedrozo <lanceroexperto@gmail.com>
2023-02-24 00:59:03 -0500

    react first task app

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useContext } from "react";
+import {TaskContext} from "../context/TaskContext";
+
+function TaskForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const {createTask} = useContext(TaskContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask({
+        title, 
+        description
+    });
+    setTitle("")
+    setDescription("")
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="escribe tu tarea"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        autoFocus
+      />
+      <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        placeholder="escribe la descripcion de la tarea"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      ></textarea>
+      <button>Guardar</button>
+    </form>
+  );
+}
+
+export default TaskForm;
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import TaskCard from "./TaskCard";
+import {TaskContext} from "../context/TaskContext";
+
+function TaskList() {
+  const {tasks} = useContext(TaskContext);
+
+  if (tasks.length === 0) {
+    return <h1>No hay tareas aun</h1>;
+  }
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+        ))}
+    </div>
+  );
+}
+
+export default TaskList;
